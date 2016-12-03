0a0f1a6c91943527c6ff85acb3ef77814a0cf17d
Sam Kozloff <samkozloff89@gmail.com>
2016-12-02 20:12:57 -0600

    moving on to next lesson, cleaning up code

--- linear_regression.py
+++ linear_regression.py
@@ -13,40 +13,40 @@ loan_length = loansData['Loan.Length'];
 interest_iter = loansData['Interest.Rate'].iteritems();
 interest_map = map(lambda x: x[1].replace('%', '', ), interest_iter)
 loansData.loc[:, 'Interest.Rate'] = pd.Series(interest_map, loansData.index)
+loansData['Interest.Rate'] = loansData['Interest.Rate'].map(lambda x : round(float(x) / 100.0, 4));
 
 length_iter = loan_length.iteritems();
 length_map = map(lambda x : int(x[1].split(' ')[0]), length_iter);
-print 'Length_map:'
 loansData.loc[:, 'Loan.Length'] = pd.Series(length_map, loansData.index)
 
 FICO = loansData['FICO.Range'];
 fico_iter = FICO.iteritems()
 y = map(lambda x : int(x[1].split('-')[0]), fico_iter);
-print y;
-
 loansData.loc[:, 'FICO.Score'] = pd.Series(y, loansData.index);
-print loansData.loc[:, 'FICO.Score'];
-print loansData['Interest.Rate'];
-print FICO.index[0];
-print length_map;
+
+print loansData['Amount.Requested'];
 
 
 loan_length_cleaned = loansData['Loan.Length'];
 interest_rate_cleaned = loansData['Interest.Rate']
 FICO_score_cleaned = loansData['FICO.Score']
 
-FICO_score_cleaned.hist();
+#Histogram
+#FICO_score_cleaned.hist();
 
+#just a counter for fun
 count = collections.Counter(FICO_score_cleaned)
-print count;
-#
+
+#These scatter matrices are cool
 # a = pd.scatter_matrix(loansData, alpha=0.05, figsize=(10,10))
-# a = pd.scatter_matrix(loansData, alpha=0.05, figsize=(10,10), diagonal='hist')
+# b = pd.scatter_matrix(loansData, alpha=0.05, figsize=(10,10), diagonal='hist')
 
 #plt.show();
 
-#you can just map on colums direclty
-print loansData[:, 10];
+# Column Headers
+# print list(loansData);
+# ['Amount.Requested', 'Amount.Funded.By.Investors', 'Interest.Rate', 'Loan.Length', 'Loan.Purpose', 'Debt.To.Income.Ratio', 'State', 'Home.Ownership', 'Monthly.Income', 'FICO.Range',
+# 'Open.CREDIT.Lines', 'Revolving.CREDIT.Balance', 'Inquiries.in.the.Last.6.Months', 'Employment.Length', 'FICO.Score']
 
 
 
